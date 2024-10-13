3a103531628eee86bd8c5287c6ddf521eb09a265
Emnu19631 <emanuel19631@gmail.com>
2024-10-13 16:34:27 -0500

    Tarea.java

--- src/principal/Tarea.java
+++ src/principal/Tarea.java
@@ -1,5 +1,34 @@
 package principal;
 
 public class Tarea {
+  private String nombreTarea;
+  private String descripcion;
+  private int prioridad;
+  private String fechaLimite;
 
+  public Tarea (String nombreTarea, String descripcion, int prioridad, String fechaLimite){
+    this.nombreTarea = nombreTarea;
+    this.descripcion = descripcion;
+    this.prioridad = prioridad;
+    this.fechaLimite = fechaLimite;
+  }
+  public String getNombreTarea(){
+    return nombreTarea;
+  }
+   public String getDescripcion(){
+    return descripcion;
+  }
+   public int getPrioridad(){
+    return prioridad;
+  }
+  public String getFechaLimite(){
+    return fechaLimite;
+  }
+  public void completarTarea(){
+    System.out.println("Tarea'"+ nombreTarea +"'completada.");
+  }
+  public void modificarTarea(String nuevaDescripcion){
+    this.descripcion= nuevaDescripcion;
+    System.out.println("Tarea'"+ nombreTarea +"'modificada.")
+  }
 }
