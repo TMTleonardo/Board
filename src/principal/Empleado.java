0ce2cb5b6131c02be8323cc60f07b14663293905
Leo <tmtleonardo@gmail.com>
2024-10-13 09:49:24 -0500

    Cambios en empleado

--- src/principal/Empleado.java
+++ src/principal/Empleado.java
@@ -2,8 +2,14 @@ package principal;
 
 public class Empleado {
 
+	String nombre;
+	String apellido;
+	String codigo;
+	String correoElectronico;
   
-  public Empleado() {
-	  
+  public Empleado(String nombre, String apellido, String codigo) {
+	  this.nombre = nombre;
+	  this.apellido = apellido;
+	  this.codigo = codigo;
   }
 }
